a8d755050409d871eb1af271a45426fe3a31d9fc
nitzan-shifrin <nshifrin1@gmail.com>
2024-12-09 20:04:07 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,17 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    mapping_codons_amino = {}
+    with open(file_path, "r") as file:
+        rows = file.readlines()
+
+    for row in rows:
+        row = row.strip()
+        cells = row.split('\t')
+
+        codons = cells[0]
+        amino_acids = cells[2]
+
+        mapping_codons_amino[codons] = amino_acids
+
+    return mapping_codons_amino
 
 
